6dcb16378c8ec771091879bbe5b513ef01c1f33b
Kom Sihon <rsihon@gmail.com>
2020-03-04 16:06:45 +0100

    BonusWallet.top_up_on field has new attribute auto_now_add=True

--- daraja/models.py
+++ daraja/models.py
@@ -142,7 +142,7 @@ class BonusWallet(Model):
 
     dara_id = models.CharField(max_length=24, unique=True)
     cash = models.IntegerField(default=0)
-    top_up_on = models.DateTimeField(db_index=True)
+    top_up_on = models.DateTimeField(db_index=True, auto_now_add=True)
 
 
 class Invitation(Model):
