8a5bcf71cb0dee394f527efd40d562c0467742e2
Kom Sihon <rsihon@gmail.com>
2020-05-20 18:28:55 +0100

    daraja.middleware.SetDaraMiddleware now sets the referrer cookie with an expires date for 30 days later

--- daraja/middleware.py
+++ daraja/middleware.py
@@ -1,4 +1,5 @@
 # -*- coding: utf-8 -*-
+from datetime import datetime, timedelta
 
 
 class SetDaraMiddleware(object):
@@ -9,5 +10,6 @@ class SetDaraMiddleware(object):
     def process_response(self, request, response):
         referrer = request.GET.get('referrer')
         if referrer:
-            response.set_cookie('referrer', referrer)
+            expires = datetime.now() + timedelta(days=30)
+            response.set_cookie('referrer', referrer, expires=expires)
         return response
