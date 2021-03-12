2ca4525b84e1137dde4aa6f3b4542dd7bf658e28
ikwen Ltd <ikwen.tech@gmail.com>
2021-03-12 12:53:53 +0100

    App made Djongo ready

--- daraja/models.py
+++ daraja/models.py
@@ -2,7 +2,7 @@ from datetime import datetime
 
 from django.conf import settings
 from django.db import transaction
-from django.utils.translation import gettext_lazy as _
+from django.utils.translation import ugettext_lazy as _
 
 from djongo import models
 
